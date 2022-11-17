2a22ac1062af6ed650ecd9aab0632fe01d73fb7d
PRDCOD002 <PRDCOD002@SHINEWING.COM>
2022-11-17 08:50:44 +0800

    add eslint、import-hint

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/vue3-essential"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        "vue/multi-word-component-names": [
+            "error",
+            {
+                ignores: ["index"],
+            }
+        ],
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-unused-vars': 1,
+        'vue/no-unused-components': 1,
+        'vue/no-mutating-props': 0,
+        'vue/html-self-closing': ['error', {
+            html: {
+                void: 'never',
+                normal: 'never',
+                component: 'always'
+            },
+            svg: 'always',
+            math: 'always'
+        }]
+    }
+}
--- src/router/permission.js
+++ src/router/permission.js
@@ -7,7 +7,6 @@ import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 import Cookie from 'js-cookie'
 import {useUserAccountStore} from 'modules/UserAccount/store'
-
 NProgress.configure({
   showSpinner: false // 关闭加载圈动画
 })
@@ -24,7 +23,7 @@ router.beforeEach(async(to,from,next)=>{
   if(to.query.token){
     Cookie.set('token',to.query.token)
     Cookie.set('name',to.query.uName)
-    return next({query:{}})
+    return next({query:{},replace: true})
   }
 
   // 1.去登陆页直接放行
@@ -44,24 +43,22 @@ router.beforeEach(async(to,from,next)=>{
     if(!error){
       Cookie.set('token',token)
       Cookie.set('name',result.userName)
+
+      // 4.如果没有菜单列表，就重新请求菜单列表并添加动态路由
+      const userCode = userAccountStore.getUserInfo.userCode
+      const {error:err,result:res} = await userAccountStore.askUserMenu({code:userCode})
+      if(!err){
+        await userAccountStore.filterPrivateRoutes(res)
+        return next()
+      }
     }else{
       ElMessage.error('身份过期，请重新登录')
       return next({ name:'ProjectLogin', replace: true });
     }
   }
 
-  // 4.如果没有菜单列表，就重新请求菜单列表并添加动态路由
-  if(!userAccountStore.getPermissionList.length){
-    const userCode = userAccountStore.getUserInfo.userCode
-    const {error,result} = await userAccountStore.askUserMenu({code:userCode})
-    if(!error){
-      await userAccountStore.filterPrivateRoutes(result)
-      return next({path:to.path})
-    }
-  }else{
-    // 5.正常访问页面
-    next()
-  }
+  // 5.直接放行
+  next()
 })
 
 // 路由拦截
